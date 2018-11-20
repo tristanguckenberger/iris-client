622bd7e542eadb8ab2b4fb2d25c466138e3c0f22
William Anderson <williamanderson@Cloverleaf-Air.Home>
2018-11-19 22:51:38 -0500

    Added text to speech and connected to API for image analysis

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+/* global fetch FormData */
 import React from 'react';
-import { Camera, Permissions } from 'expo';
+import { Camera, Permissions, Speech } from 'expo';
 import { Text, View, TouchableOpacity, Vibration } from 'react-native';
+
+const API_URL = 'http://max-image-caption-generator.max.us-south.containers.appdomain.cloud/model/predict';
 
 export default class App extends React.Component {
   state = {
@@ -12,13 +15,47 @@ export default class App extends React.Component {
     this.setState({ hasCameraPermission: status === 'granted' });
   }
 
-  handleTouchPress = () => {
-    console.debug('Touch press');
-    Vibration.vibrate();
+  capturePhoto = async () => {
+    Speech.speak('Capturing');
+
+    if (this.camera) {
+      const photo = await this.camera.takePictureAsync();
+      return photo;
+    }
+
+    return null;
+  };
+
+  getDescription = async (imageUri) => {
+    const uriParts = imageUri.split('.');
+    const fileType = uriParts[uriParts.length - 1];
+
+    const formData = new FormData();
+    formData.append('image', {
+      uri: imageUri,
+      name: 'image.jpeg',
+      type: `image/${fileType}`,
+    });
+
+    // Send the request
+    return fetch(API_URL, {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    })
+      .catch(error => console.error('Error:', error));
   }
 
-  handleTouchRelease = () => {
-    console.debug('Touch release');
+  handleTouchPress = async () => {
+    Vibration.vibrate(100);
+    const photo = await this.capturePhoto();
+    const descriptionResponse = await this.getDescription(photo.uri);
+    const descriptionResponseJSON = await descriptionResponse.json();
+    console.debug('JSON Response: ', descriptionResponseJSON);
+    const description = descriptionResponseJSON.predictions[0].caption;
+    Speech.speak(description);
   }
 
   render() {
@@ -35,14 +72,15 @@ export default class App extends React.Component {
       <View
         style={{ flex: 1 }}
         onResponderGrant={this.handleTouchPress}
-        onResponderRelease={this.handleTouchRelease}
         onStartShouldSetResponder={() => true}
       >
         <Camera
           style={{ flex: 1 }}
+          ref={(ref) => {
+            this.camera = ref;
+          }}
           type={Camera.Constants.Type.back}
           autoFocus={Camera.Constants.AutoFocus.on}
-          onCameraReady={() => console.log('Camera ready')}
           ratio="16:9"
         >
           <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
